9fdf60f2933e097af07ee367c5ae7af3b7f49aab
AndresPadillaUcros <you@example.comaandres.padilla.u@outlook.com>
2022-05-23 09:34:10 -0500

    httLink HTTPS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,8 +29,8 @@ const client = new ApolloClient({
     new HttpLink({
       uri:
         process.env.NODE_ENV === 'production'
-          ? 'http://gestor-proyectos.vercel.app/api/graphql'
-          : 'http://gestor-proyectos.vercel.app/api/graphql',
+          ? 'https://gestor-proyectos.vercel.app/api/graphql'
+          : 'https://gestor-proyectos.vercel.app/api/graphql',
     })
   ])
 })
